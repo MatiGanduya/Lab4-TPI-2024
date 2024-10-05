977b02a6277035baeb37a1f0cc61410f38179bf0
Matias Ganduya <matiasganduya@gmail.com>
2024-10-05 12:18:33 -0300

    Creacion de la tabla appoinments

--- app/Models/appointments.php
+++ app/Models/appointments.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class servicio extends Model
+class appointments extends Model
 {
     use HasFactory;
 }
