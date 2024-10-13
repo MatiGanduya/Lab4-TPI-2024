be9cd531f044746c372090db439fe44fbb8bd5bf
Blas Cabrera <blascabrera78@gmail.com>
2024-10-13 18:55:17 -0300

    Implementacion de plantilla

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <title>Menú con Barra Negra</title>
+</head>
+
+<body>
+    <div class="welcome_view">
+        <div class="header">
+            <div class="rectangle1"></div>
+            <button class="iniciarsesión">Iniciar sesión</button>
+            <button class="registrarse">Registrarse</button>
+        </div>
+        <div class="todoturnos">Todo Turnos</div>
+
+        <div class="tutiempoesloquemasnosimporta">Tu tiempo es lo que más nos importa</div>
+        <button class="turnos2">Turnos</button>
+        <button class="home">Home</button>
+    </div>
+
+    </div>
+</body>
+
+</html>
