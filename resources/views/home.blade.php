35db9684698d0be8bcb52091315399a456cb4d4f
darioBD <dario.d.monte.d.o@gmail>
2024-10-23 14:33:50 -0300

    botones del header y home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,8 @@
         <div class="d-grid gap-3">
             <a href="{{ Route('servicios.indexServicios') }}" class="btn btn-primary btn-lg">Solicitar un turno</a>
             <a href="#" class="btn btn-primary btn-lg">Mis turnos</a>
-            <a href="{{ Route('empresa.indexEmpresa') }}" class="btn btn-info btn-lg">Mi Empresa y Servicios</a>
+            <a href="{{ Route('empresa.indexEmpresa') }}" class="btn btn-primary btn-lg">Mi empresa y servicios</a>
+            <a href="#" class="btn btn-primary btn-lg">Mi disponibilidad</a>
         </div>
     </div>
 </div>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -25,8 +25,10 @@
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                    <li><a href="{{ url('/') }}" class="btn btn-outline-light me-2">Home</a></li>
                     <li><a href="{{ Route('servicios.indexServicios') }}" class="btn btn-outline-light me-2">Turnos</a></li>
+                    @auth
                     <li><a href="{{ Route('empresa.indexEmpresa') }}" class="btn btn-outline-light me-2">Mi Empresa y Servicios</a></li>
                     <li><a href="#" class="btn btn-outline-light me-2">Mi disponibilidad</a></li>
+                    @endauth
                 </ul>
 
                 <div class="text-end">
